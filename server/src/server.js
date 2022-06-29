7d8916c16b90343192a1b47d05e6e58c68a322d7
Levi Fry <levifry@Levis-MacBook-Air.local>
2022-06-28 20:18:01 -0700

    fixes

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,14 @@ import knex from "../db/db.js";
 
 const server = express();
 
-var opts = {
-  origin: ["http://localhost:3000", "https://smartsheets-client.herokuapp.com/"],
-  credentials: true,
-}
+// var opts = {
+//   origin: ["http://localhost:3000","http://76.174.184.10:3000","http://localhost:1337/","http://76.174.184.10:1337","http://live.levifry.com"],
+//   optionsSuccessStatus: 200,
+//   methods: ["GET","POST","DELETE","UPDATE", "PUT", "PATCH"],
+//   credentials: true,
+// }
 
-server.use(cors(opts));
+server.use(cors());
 server.use(json());
 
 server.get('/api/', (request, response) => {
