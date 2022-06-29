22e9fe632b37c2f6a96d3fd54e93fce815aec098
Levi Fry <levifry@Levis-MacBook-Air.local>
2022-06-28 18:08:54 -0700

    fixes

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,24 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
+import knex from 'knex';
+import knexfile from '../knexfile';
+
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+    origin: ["http://localhost:3000", "https://smartsheets-client.herokuapp.com/"],
+    optionsSuccessStatus: 200,
+    methods: ["GET","POST","DELETE","UPDATE", "PUT", "PATCH"],
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
+app.use(json());
 
 const env = process.env.NODE_ENV || 'development'
-const config = require('../knexfile')[env]
-const knex = require('knex')(config)
+const config = knexfile[env]
+const db = knex(config);
+
 
 app.get('/', (request, response) => {
     response.set("Access-Control-Allow-Origin", "*");
@@ -14,7 +26,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/authors', (request, response) => {
-    knex('app_authors')
+    db('app_authors')
         .select('*')
         .then(authorRecords => {
             let responseData = authorRecords.map(author => ({ firstName: author.first_name, lastName: author.last_name}));
@@ -22,6 +34,7 @@ app.get('/authors', (request, response) => {
         })
 
 })
+
 
 export default app;
 
