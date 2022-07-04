5250d0bff462ed563a62d6487dea7adc95fbee16
Kyle Dilick <kdilick@gmail.com>
2022-07-04 16:50:09 -0600

    added new entry button, modified EntryDetails to handle a new entry state

--- client/src/_context/SheetProvider.jsx
+++ client/src/_context/SheetProvider.jsx
@@ -1,22 +1,26 @@
 import React, { createContext } from 'react'
 import useCurrentSheet from './states/useCurrentSheet'
 import useSelectedEntry from './states/useSelectedEntry'
+import useNewEntry from './states/useNewEntry'
 
 const SheetContext = createContext()
 
 const SheetProvider = ({ children }) => {
   const { currentSheet, setCurrentSheet } = useCurrentSheet();
   const { selectedEntry, setSelectedEntry } = useSelectedEntry();
-  
+  const { newEntry, setNewEntry } = useNewEntry();
+
   const sheet = {
 
     /* STATES */
     currentSheet,
     selectedEntry,
+    newEntry,
 
     /* SETTERS */
     setCurrentSheet,
     setSelectedEntry,
+    setNewEntry,
 
     /* EFFECTS */
 
