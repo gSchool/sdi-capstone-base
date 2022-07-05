2f5c9d11caaceb7ea4a677aafbd1f8e949c9ddb6
Nehemiah <ralph.1996.3@gmail.com>
2022-07-05 10:10:02 -0600

    initial config

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,21 +4,58 @@ import config from './config'
 const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 function App() {
-
-  let [names, setNames] = useState([ ]);
-
-  useEffect(() => {
-    fetch(ApiUrl + "/authors")
-      .then(response => response.json())
-      .then(data => setNames(data))
-      .catch(err => console.log(err))
-  }, []);
-
+  
+  const userId = 1 //change these to match the actual logged in user
+  const isAdmin = false
 
   return (
     <div>
-      App is running - good work: 
-      { names.map(author => author.firstName + " ")}
+      {
+        isAdmin ? 
+
+          <Router>
+            <Header/>
+              <Routes>
+                <Route path = '/tasks' element ={<Dashboard user = {false}/>}> </Route>
+                <Route path = '/tasks/:user' element ={<Dashboard user = {false}/>}> </Route>
+                <Route path = '/tasks/add' element ={<AddPage type = {"task"}/>}> </Route>
+                <Route path = '/tasks/details/:task' element ={<TaskDetails/>}> </Route>
+                <Route path = '/tasks/weekly' element ={<TaskTable/>}> </Route>
+                <Route path = '/tasks/weekly/:user' element ={<TaskTable/>}> </Route>
+                <Route path = '/profile' element ={<Profile/>}> </Route>
+                <Route path = '/menu' element ={<AdminMenu/>}> </Route>
+                <Route path = '/allO' element ={<Profile/>}> </Route>
+                <Route path = '/*' element = {<Posts user = {false}/>}></Route>
+              </Routes>
+          </Router>
+        :
+
+        userId !== null ?
+            <Router>
+            <Header/>
+              <Routes>
+                <Route path = '/tasks' element ={<Dashboard user = {false}/>}> </Route>
+                <Route path = '/tasks/:user' element ={<Dashboard user = {false}/>}> </Route>
+                <Route path = '/tasks/add' element ={<AddPage type = {"task"}/>}> </Route>
+                <Route path = '/tasks/details/:task' element ={<TaskDetails/>}> </Route>
+                <Route path = '/tasks/weekly' element ={<TaskTable/>}> </Route>
+                <Route path = '/tasks/weekly/:user' element ={<TaskTable/>}> </Route>
+                <Route path = '/profile' element ={<Profile/>}> </Route>
+                <Route path = '/*' element = {<Posts user = {false}/>}></Route>
+              </Routes>
+          </Router>
+
+          :
+
+          <Router>
+            <Header/>
+            <Routes>
+                <Route path = '/' element ={<SignUp/>}> </Route>
+                <Route path = '/login' element ={<Login/>}> </Route>
+                <Route path = '/*' element = {<Login/>}></Route>
+          </Routes>
+        </Router>
+      }
     </div>
   );
 }
