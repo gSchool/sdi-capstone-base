31a7b058aa194064b833f05712c605bf1739eff5
Cybyl Jade <cybyl.hancock@us.af.mil>
2022-09-08 21:16:56 -0600

    fix: tests

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders blog title', () => {
   render(<App />);
-  const title = screen.getByText('BLOG');
+  const title = screen.getByText('GAME NIGHT');
   expect(title).toBeInTheDocument();
 });
-
-// test('renders learn react link', () => {
-//   expect(1).toBe(1);
-// });
