0bd96115edd880438a60d543be7341bfae751bf0
Jeff Haddock <jeff.haddock.developer@gmail.com>
2022-12-06 11:12:01 -0500

    removes console.log

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState} from 'react';
 import config from './config'
 
-console.log("Ye OLDE PROCESS:", process.env);
 const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 function App() {
