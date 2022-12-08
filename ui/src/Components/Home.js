4b13324d00b05a1dab313cca5d21c94de7c3f9c3
Danny <rommatt4@gmail.com>
2022-12-08 16:13:34 -0500

    ready for that DB

--- ui/src/Components/Home.js
+++ ui/src/Components/Home.js
@@ -1,20 +1,24 @@
-import '../App.css';
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Header1 from './Header1';
+import "../App.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import Header1 from "./Header1";
+import logo from "../1.jpg";
 
 function Home() {
-    return (
-        <>
-        <Header1/>
-        <div>
-            <Link to={`/Assets/1`}>
-            <img src= "img/1.jpg" alt="alt"></img>
-            <h1>This is where asset types will be displayed as images, and the image will be clickable links to assets page</h1>
-            </Link>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <Header1 />
+      <div>
+        <Link to={`/Assets/1`}>
+          <img src={logo} alt="alt"></img>
+          <h1>
+            This is where asset types will be displayed as images, and the image
+            will be clickable links to assets page
+          </h1>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Home;
