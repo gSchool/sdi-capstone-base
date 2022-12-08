f06ee96900145e5a2a5b19010286d10dad28de59
Danny <rommatt4@gmail.com>
2022-12-08 16:22:06 -0500

    ready for the DB round 2

--- ui/src/views/AssetView.js
+++ ui/src/views/AssetView.js
@@ -1,40 +1,3 @@
-// import React from "react";
-// import Card from "react-bootstrap/Card";
-// import Button from "react-bootstrap/Button";
-// import ButtonGroup from "react-bootstrap/ButtonGroup";
-// import axios from "axios";
-//
-
-// export default function AssetView() {
-//   const handleRemove = () => {};
-
-//   const handleRequest = () => {};
-
-//   return (
-//     <>
-//       <Header />
-//       <Card style={{ width: "18rem" }}>
-//         <Card.Img
-//           variant="top"
-//           src="https://c.files.bbci.co.uk/AD05/production/_123139244_reaper_getty.jpg"
-//         ></Card.Img>
-//         <Card.Body>
-//           <Card.Title>This is a title</Card.Title>
-//           <Card.Text>This is a placeholder</Card.Text>
-//           <ButtonGroup>
-//             <Button variant="outline-danger" onClick={() => handleRemove()}>
-//               Remove
-//             </Button>
-//             <Button variant="outline-success" onClick={() => handleRequest()}>
-//               Request
-//             </Button>
-//           </ButtonGroup>
-//         </Card.Body>
-//       </Card>
-//     </>
-//   );
-// }
-
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
@@ -74,20 +37,9 @@ export default function AssetView() {
             </Typography>
           </CardContent>
           <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-            <IconButton aria-label="previous">
-              {theme.direction === "rtl" ? (
-                <AddCircleRoundedIcon />
-              ) : (
-                <RemoveCircleRoundedIcon />
-              )}
-            </IconButton>
-            <IconButton aria-label="play/pause"></IconButton>
-            <IconButton aria-label="next">
-              {theme.direction === "rtl" ? (
-                <RemoveCircleRoundedIcon />
-              ) : (
-                <AddCircleRoundedIcon />
-              )}
+            <IconButton aria-label="add">
+              <RemoveCircleRoundedIcon fontSize="large" color="error" />
+              <AddCircleRoundedIcon fontSize="large" color="success" />
             </IconButton>
           </Box>
         </Box>
