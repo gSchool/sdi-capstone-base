66e90364cf0560fab4e993d8a30165308ddcb1ec
Matt <mfouquier1@gmail.com>
2022-12-09 11:26:31 -0500

    added patch to  request table

--- api/src/crud/approvals.js
+++ api/src/crud/approvals.js
@@ -14,18 +14,17 @@ const knex = require('knex')(config)
 router.get('/', async (req, res) => {
   try {
     let requestList = await knex
-      .select('request.id as Request_ID', 'date', 'location', 'mission_title', 'justification', 'status', 'all_users.first_name as User_first', 'all_users.last_name as User_last', 'sme_approver.first_name as SME_first', 'sme_approver.last_name as SME_last', 'cmd_approver.first_name as CMD_first', 'cmd_approver.last_name as CMD_last', 'sme_approver.id AS SME_ID', 'cmd_approver.id as CMD_ID', 'all_users.id as USER_ID')
+      .select('request.id as Request_ID', 'date', 'location', 'mission_title', 'justification', 'status', 'all_users.first_name as User_first', 'all_users.last_name as User_last', 'sme_approver.first_name as SME_first', 'sme_approver.last_name as SME_last', 'cmd_approver.first_name as CMD_first', 'cmd_approver.last_name as CMD_last', 'sme_approver.id AS SME_ID', 'cmd_approver.id as CMD_ID', 'all_users.id as USER_ID', 'asset.type', 'asset.asset_name')
       .from('request')
       .innerJoin('all_users', 'request.user_id', 'all_users.id')
       .innerJoin('sme_approver', 'sme_approver.id', 'request.sme_id')
       .innerJoin('cmd_approver', 'cmd_approver.id', 'request.cmd_id')
+      .innerJoin('asset', 'asset.id', 'request.asset_id')
     res.status(200).send(requestList)
   } catch (err) {
     console.log('Error fetching requests: '(err));
   }
 })
-
-
 
 
 router.patch('/:id', async (req, res) => {
