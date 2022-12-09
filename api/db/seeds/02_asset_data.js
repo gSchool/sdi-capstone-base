560939bdaad84f160a11817399ddaabacd68bef3
Matt <mfouquier1@gmail.com>
2022-12-09 14:28:44 -0500

    fix knexfile

--- api/db/seeds/02_asset_data.js
+++ api/db/seeds/02_asset_data.js
@@ -7,12 +7,12 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('asset').insert([
-    { id: 1, asset_name: 'Satellite', description: 'Powered by on-board processing and resilient connectivity, this tactical ISR satellite line enables in-theater, low-latency sensor tasking, on-orbit processing of mission data, protected communications and direct downlink of situational awareness and targeting information, increasingly essential to shortening the sensor-to-shooter timeline against fleeting targets.', type: 'ISR', image_url: '../img/isr/satellite.jpg' },
-    { id: 2, asset_name: 'Aerial', description: 'ISR and EW platforms provide modern and advanced integration techniques fusing on-board and off-board collection and targeting data into customized airborne mission system solutions.', type: 'ISR', image_url: '../img/isr/aerial.jpg' },
-    { id: 3, asset_name: 'Satcom', description: '', type: 'Communications', image_url: '../img/comms/satcom.jpg' },
-    { id: 4, asset_name: 'Networking', description: '', type: 'Communications', image_url: '../img/comms/networking.jpg' },
-    { id: 5, asset_name: 'Mobile Device', description: '', type: 'Communications', image_url: '../img/comms/cellphone.jpg' },
-    { id: 6, asset_name: 'Desktop', description: '', type: 'Communications', image_url: '../img/comms/desktop.jpg' },
+    { id: 1, asset_name: 'Satellite', description: 'Powered by on-board processing and resilient connectivity, this tactical ISR satellite line enables in-theater, low-latency sensor tasking, on-orbit processing of mission data, protected communications and direct downlink of situational awareness and targeting information, increasingly essential to shortening the sensor-to-shooter timeline against fleeting targets.', type: 'ISR', image_url: 'https://wallpapercave.com/wp/wp3261494.jpg' },
+    { id: 2, asset_name: 'Aerial', description: 'ISR and EW platforms provide modern and advanced integration techniques fusing on-board and off-board collection and targeting data into customized airborne mission system solutions.', type: 'ISR', image_url: 'https://static.dw.com/image/54071362_1006.jpg' },
+    { id: 3, asset_name: 'Satcom', description: '', type: 'Communications', image_url: 'https://www.getsat.com/wp-content/uploads/2020/05/HBH_5422_resize-768x509.jpg' },
+    { id: 4, asset_name: 'Networking', description: '', type: 'Communications', image_url: 'https://api.army.mil/e2/c/images/2017/10/17/495732/size0-full.jpg' },
+    { id: 5, asset_name: 'Mobile Device', description: '', type: 'Communications', image_url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTIf_obnebJ2Hu7jakchxUp8TMAV0lP-fU_zBNXTX0-qu8WlvNn' },
+    { id: 6, asset_name: 'Desktop', description: '', type: 'Communications', image_url: 'https://www.stealth.com/wp-content/uploads/2020/07/SBXI-173-front1-web.jpg' },
     { id: 7, asset_name: 'CSfC', description: '', type: 'Communications', image_url: '' },
     { id: 8, asset_name: 'Fixed Wing', description: '', type: 'Transportation', image_url: '' },
     { id: 9, asset_name: 'Rotary Wing', description: '', type: 'Transportation', image_url: '' },
