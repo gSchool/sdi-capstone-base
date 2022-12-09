95dbacf5b41ee141d5be990e3a389759f5747c4a
Matt <mfouquier1@gmail.com>
2022-12-09 11:12:15 -0500

    added patch to  request table

--- api/src/crud/approvals.js
+++ api/src/crud/approvals.js
@@ -14,7 +14,7 @@ const knex = require('knex')(config)
 router.get('/', async (req, res) => {
   try {
     let requestList = await knex
-      .select('date', 'location', 'mission_title', 'justification', 'status', 'all_users.first_name as User_first', 'all_users.last_name as User_last', 'sme_approver.first_name as SME_first', 'sme_approver.last_name as SME_last', 'cmd_approver.first_name as CMD_first', 'cmd_approver.last_name as CMD_last', 'sme_approver.id AS SME_ID', 'cmd_approver.id as CMD_ID', 'all_users.id as USER_ID')
+      .select('request.id as Request_ID', 'date', 'location', 'mission_title', 'justification', 'status', 'all_users.first_name as User_first', 'all_users.last_name as User_last', 'sme_approver.first_name as SME_first', 'sme_approver.last_name as SME_last', 'cmd_approver.first_name as CMD_first', 'cmd_approver.last_name as CMD_last', 'sme_approver.id AS SME_ID', 'cmd_approver.id as CMD_ID', 'all_users.id as USER_ID')
       .from('request')
       .innerJoin('all_users', 'request.user_id', 'all_users.id')
       .innerJoin('sme_approver', 'sme_approver.id', 'request.sme_id')
@@ -26,12 +26,18 @@ router.get('/', async (req, res) => {
 })
 
 
-//router.get('/', async (req, res) => {
-//   try {
-//     let requestList = await knex
-//       .select('date', 'location', 'mission_title', 'justification', 'status').from('request')
-//     res.status(200).send(requestList)
-//   } catch (err) {
-//     console.log('Error fetching requests: '(err));
-//   }
-// })
+
+
+router.patch('/:id', async (req, res) => {
+  const updatedId = parseInt(req.params.id);
+  try {
+    let updatedRequest = {
+      'status': req.body.status
+    }
+    let updatedRequestKnex = await knex('request').where('id', updatedId).update(updatedRequest);
+    res.status(200).send('Request Updated')
+  }
+  catch (e) {
+    console.log('Error in patching unit:', e);
+  }
+})
