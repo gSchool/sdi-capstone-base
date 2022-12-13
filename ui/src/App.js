d07d23c8198a4b8bc8d2f175ebdee3b988339a9b
Ryan Kirk <ryanspacekirk@gmail.com>
2022-12-13 11:33:05 -0600

    react functioning

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,47 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import config from './config'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import CrewProfile from './pages/CrewProfile';
-import LeaderProfile from './pages/LeaderProfile';
-import Signup from './pages/Signup';
-import Splash from './pages/Splash';
-
-
-const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  let [autehnticatedUser, setAuthenticatedUser] = useState({});
-
-  useEffect(() => {
-    // fetch(ApiUrl + '/data')
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     setData(data)
-    //   })
-  }, [])
-
-
   return (
-    <div className='App'>
-      <Context.Provider value={{ autehnticatedUser, setAuthenticatedUser }}>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/splash" element={<Splash />} />
-            <Route path="/crewProfile" element={<CrewProfile />} />
-            <Route path="/leaderProfile" element={<LeaderProfile />} />
-          </Routes>
-        </BrowserRouter>
-      </Context.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-
   );
 }
 
 export default App;
-
-export const Context = React.createContext();
