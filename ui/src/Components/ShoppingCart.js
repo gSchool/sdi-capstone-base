ee63a98a5dbb80756f9a36c4d737f70a7e93757f
Danny <rommatt4@gmail.com>
2022-12-13 12:46:24 -0500

    before lunch stuff

--- ui/src/Components/ShoppingCart.js
+++ ui/src/Components/ShoppingCart.js
@@ -1,23 +1,17 @@
-import React, { useEffect } from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
 
 export default function ShoppingCart() {
-
-
-  const cartItems = JSON.parse(localStorage.getItem('cartInfo'));
-  console.log('I am shopping cart data', cartItems)
+  const cartItems = JSON.parse(localStorage.getItem("cartInfo"));
+  console.log("I am shopping cart data", cartItems);
 
   return (
     <>
       <Header />
       <h1>Review Your Cart</h1>
       {cartItems.map((item, idx) => (
-        <div key={idx}>
-          {/* {console.log('I am the item', item)} */}
-
-        </div >
-      ))
-      }
+        <div key={idx}>{/* {console.log('I am the item', item)} */}</div>
+      ))}
     </>
-  )
+  );
 }
