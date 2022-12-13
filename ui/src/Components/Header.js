9e320df6bf69834051c47b6557173b3ae472d5de
Danny <rommatt4@gmail.com>
2022-12-12 20:29:09 -0500

    end of monday commit

--- ui/src/Components/Header.js
+++ ui/src/Components/Header.js
@@ -1,45 +1,50 @@
-
-import '../App.css';
-import React from 'react'
-import { Link } from 'react-router-dom';
-import SideMenu from './SideMenu';
-import {  MdOutlineShoppingCart  } from 'react-icons/md'
-import Badge from '@material-ui/core/Badge'
-import logo from '../img/logo.png'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-import Button from 'react-bootstrap/Button';
+import "../App.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import SideMenu from "./SideMenu";
+import { MdOutlineShoppingCart } from "react-icons/md";
+// import Badge from '@material-ui/core/Badge'
+import logo from "../img/logo.png";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import Button from "react-bootstrap/Button";
 
 function Header() {
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Click here to complete your requests.
+    </Tooltip>
+  );
 
-    const renderTooltip = (props) => (
-        <Tooltip id="button-tooltip" {...props}>
-            Click here to complete your requests.
-        </Tooltip>
-    );
-
-    return (
-        <div className="Header">
-            <div className="grid1">
-                <SideMenu />
-            </div>
-            <div className="grid2">
-                <Link to={`/Home`}>
-                    <img src={logo} alt="alt" />
-                </Link>
-            </div>
-            <div className="grid3">
-                <Link to={`/Requests`}>
-                    <OverlayTrigger placement="bottom" delay={{ show: 250, hide: 400 }} overlay={renderTooltip}>
-                        <Button variant="success">
-                            <MdOutlineShoppingCart style={{ color: "black", width: "38px", height: "38px" }} />
-                            1 {/*this number will dynamically display the number of things in your shopping cart*/}
-                        </Button>
-                    </OverlayTrigger>
-                </Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className="Header">
+      <div className="grid1">
+        <SideMenu />
+      </div>
+      <div className="grid2">
+        <Link to={`/Home`}>
+          <img src={logo} alt="alt" />
+        </Link>
+      </div>
+      <div className="grid3">
+        <Link to={`/Requests`}>
+          <OverlayTrigger
+            placement="bottom"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip}
+          >
+            <Button variant="success">
+              <MdOutlineShoppingCart
+                style={{ color: "black", width: "38px", height: "38px" }}
+              />
+              1{" "}
+              {/*this number will dynamically display the number of things in your shopping cart*/}
+            </Button>
+          </OverlayTrigger>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
