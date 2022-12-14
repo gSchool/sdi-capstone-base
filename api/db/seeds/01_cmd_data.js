082e34c972973890880e3b46e572b201fe73b0a4
Danny <rommatt4@gmail.com>
2022-12-14 15:36:39 -0500

    added logo to approver and sme page

--- api/db/seeds/01_cmd_data.js
+++ api/db/seeds/01_cmd_data.js
@@ -1,9 +1,9 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
 const createFakeUsers = () => {
   const fakeUsers = [];
@@ -13,21 +13,19 @@ const createFakeUsers = () => {
       first_name: faker.name.firstName(),
       last_name: faker.name.lastName(),
       username: faker.internet.userName(),
-      unit: faker.helpers.arrayElement(['A Co', 'B Co', 'C Co', 'D Co']),
-      position: 'Commander',
+      unit: faker.helpers.arrayElement(["A Co", "B Co", "C Co", "D Co"]),
+      position: "Commander",
       password: faker.internet.password(),
-      phone_number: faker.phone.number('813-###-###'),
+      phone_number: faker.phone.number("813-###-###"),
       email: faker.internet.email(),
-      type: 'Commander',
-    }
-    fakeUsers.push(user)
+      type: "Commander",
+    };
+    fakeUsers.push(user);
   }
   return fakeUsers;
-}
+};
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('cmd_approver').del()
-  await knex('cmd_approver').insert(
-    createFakeUsers()
-  );
+  await knex("cmd_approver").del();
+  await knex("cmd_approver").insert(createFakeUsers());
 };
