3fc93b7920a24782608baa1846e66ff8bdd3b66e
Danny <rommatt4@gmail.com>
2022-12-18 17:35:33 -0500

    added features to the map and changed some some data in the request table (countries)

--- api/db/seeds/05_request_data.js
+++ api/db/seeds/05_request_data.js
@@ -7,7 +7,7 @@ const { faker } = require("@faker-js/faker");
 const createFakeRequests = () => {
   const fakeRequests = [];
 
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < 6; i++) {
     const request = {
       id: i,
       date: faker.date.soon(90, "2022-12-10T00:00:00:.000Z"),
@@ -35,7 +35,6 @@ const createFakeRequests = () => {
         "Rejected",
       ]),
       user_id: faker.helpers.arrayElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-
 
       asset_id: faker.helpers.arrayElement([
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
