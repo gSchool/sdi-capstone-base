0e3255ba5817b4c92885447c114535dffc13caeb
Matt <mfouquier1@gmail.com>
2022-12-20 11:34:16 -0500

    fixed hover in assets

--- ui/src/views/AssetCard.js
+++ ui/src/views/AssetCard.js
@@ -3,33 +3,34 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import './AssetView.css'
 import { IconButton, CardActionArea, CardActions } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const AssetCard = ({ asset, handleAdd }) => {
 
   return (
-    <Card elevation={3} style={{ height: '100%', backgroundColor: "#BAAC9Dff", marginTop: 25 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          image={asset.image_url}
-          alt={asset.description}
-          style={{ height: "300px" }}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {asset.asset_name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {asset.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+    <Card className="assetCard" elevation={3} style={{ height: '100%', backgroundColor: "#BAAC9Dff", marginTop: 25 }}>
+
+      <CardMedia
+        component="img"
+        image={asset.image_url}
+        alt={asset.description}
+        style={{ height: "300px" }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {asset.asset_name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {asset.description}
+        </Typography>
+      </CardContent>
+
       <CardActions>
         <IconButton size="large" style={{ color: "#904E55" }}
           onClick={(e) => { handleAdd(e, asset); }}>
-          <AddShoppingCartIcon style={{ height: "35px", width: "35px"}}/>
+          <AddShoppingCartIcon style={{ height: "35px", width: "35px" }} />
         </IconButton>
       </CardActions>
     </Card>
